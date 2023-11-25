cd2718599441f1da0802d6054732940360ecfe95
Hajime Hoshi <hajimehoshi@gmail.com>
2023-11-25 16:20:30 +0900

    add color saturation

--- oklab.go
+++ oklab.go
@@ -39,6 +39,31 @@ func (c Oklab) RGBA() (uint32, uint32, uint32, uint32) {
 	b := toNonLinear(b_)
 	a := c.Alpha
 
+	if r < 0 {
+		r = 0
+	}
+	if r > 1 {
+		r = 1
+	}
+	if g < 0 {
+		g = 0
+	}
+	if g > 1 {
+		g = 1
+	}
+	if b < 0 {
+		b = 0
+	}
+	if b > 1 {
+		b = 1
+	}
+	if a < 0 {
+		a = 0
+	}
+	if a > 1 {
+		a = 1
+	}
+
 	return uint32(a * r * 0xffff), uint32(a * g * 0xffff), uint32(a * b * 0xffff), uint32(a * 0xffff)
 }
 
